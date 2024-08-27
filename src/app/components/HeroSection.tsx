e2fdb2ccf9e76aa82ff3e68369b167bdf5704d6b
sofia_almroth <almrothsogfia@gmail.com>
2024-08-27 13:45:02 +0200

    init portfolio

---
+++ src/app/components/HeroSection.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+function HeroSection() {
+  return (
+    <section className="min-h-screen bg-[#FFCAF2] text-black flex items-center justify-center">
+      <div className="w-full p-6 text-left">
+        <h1 className="text-[10rem] font-bold mb-[-5rem]">Hello, I'm</h1>
+        <p className="text-[12rem] font-extrabold">Sofia Almroth</p>
+      </div>
+    </section>
+  );
+}
+
+export default HeroSection;
